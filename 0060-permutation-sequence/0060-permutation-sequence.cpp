1119fe945a83952f0244508c0ec33bc31e09e115
Monika Kumari <165769316+Monika-KU@users.noreply.github.com>
2026-08-01 12:49:27 +0530

    Time: 2 ms (34.52%), Space: 8.9 MB (36.67%) - LeetHub

---
+++ 0060-permutation-sequence/0060-permutation-sequence.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    string getPermutation(int n, int k) {
+        vector<int> nums;
+        int fact = 1;
+
+        for (int i = 1; i < n; i++) {
+            fact *= i;
+            nums.push_back(i);
+        }
+        nums.push_back(n);
+
+        k--; // Convert to 0-based index
+
+        string ans = "";
+
+        while (!nums.empty()) {
+            int index = k / fact;
+
+            ans += to_string(nums[index]);
+            nums.erase(nums.begin() + index);
+
+            if (nums.empty())
+                break;
+
+            k %= fact;
+            fact /= nums.size();
+        }
+
+        return ans;
+    }
+};
